2a22d4207a4fa0e1c7341086254bfa4de70668cc
Soli Soli <solmazsm2020@gmail.com>
2022-09-26 21:22:46 -0700

    first commit

---
+++ CS380_Lab0.java
@@ -0,0 +1,11 @@
+package packagetest;
+
+public class CS380_Lab0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Lab CS 380");
+
+	}
+
+}
